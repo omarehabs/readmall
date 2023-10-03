ba42ce512597c68b75ff5f727e1e70f68ba4f2a4
omarehab <omarehabsal@gmail.com>
2023-10-03 22:54:03 +0300

    this is so weird:

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('./config/env.confic');
-const sequelize = require('./config/dbConfig');
-const helmet = require('helmet');
-const app = require('./app');
+require("./config/env.confic");
+const sequelize = require("./config/dbConfig");
+const helmet = require("helmet");
+const app = require("./app");
 const PORT = process.env.PORT;
 
 const {
@@ -16,33 +16,33 @@ const {
   Review,
   Cart,
   Favorite,
-  UserBook
-} = require('./models/models');
+  UserBook,
+} = require("./models/models");
 
-
-const multer = require('multer');
-const bodyParser = require('body-parser');
-
+const multer = require("multer");
+const bodyParser = require("body-parser");
 
 // Multer configuration to handle file uploads
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
 // Body-parser middleware to parse request body
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
 
-
 app.listen(PORT, async () => {
-  const b  = await Cart.findAll({limit: 1})
-  console.log(b)
-  const r = await Favorite.findAll({limit: 1})
-  console.log(r)
-  const o = await UserBook.findAll({limit: 1})
-  console.log(o)
-  console.log(`server is up on port ${PORT}!`);
-  await sequelize.authenticate();
-  await sequelize.sync({
-    alter: true
-  });
-
+  try {
+    const b = await Cart.findAll({ limit: 1 });
+    console.log(b);
+    const r = await Favorite.findAll({ limit: 1 });
+    console.log(r);
+    const o = await UserBook.findAll({ limit: 1 });
+    console.log(o);
+    console.log(`server is up on port ${PORT}!`);
+    await sequelize.authenticate();
+    await sequelize.sync({
+      alter: true,
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
